1defc066e425ce0f5ec77d1ba9611e0cbac4a7dc
Rishikesh Negi <rishinegi97@gmail.com>
2025-10-22 12:13:44 +0530

    Removal of sensitive information from the API service file

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://tqxbsahsvufiwlyfkwws.supabase.co";
+export const supabaseUrl = "<your-supabase-db-url-here>";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxeGJzYWhzdnVmaXdseWZrd3dzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY5NzI2NzAsImV4cCI6MjA3MjU0ODY3MH0.-lQ_4Dl3UDM0HOw39eFEY9RsmqmIhE4wbIuFL8apeGk";
+  "<your-supabase-api-key-here>";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
