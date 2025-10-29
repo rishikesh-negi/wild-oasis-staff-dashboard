5172ab9f2fdd2a01de4bc1aaafdef72cd050c816
Rishikesh Negi <rishinegi97@gmail.com>
2025-10-29 12:56:53 +0530

    Stop tracking supabase.js

--- src/services/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabaseUrl = "<your-supabase-db-url-here>";
-const supabaseKey =
-  "<your-supabase-api-key-here>";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
